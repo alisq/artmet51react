3deeb5033c68ba3a22e18b9339c00c53d81dbca6
alisq <ali.shamas.qadeer@gmail.com>
2025-09-05 11:37:33 +0100

    added build

--- src/components/artist.jsx
+++ src/components/artist.jsx
@@ -10,19 +10,14 @@ function Artist({index, title, artist, id, year, medium, dimensions, image}) {
       onMouseLeave={() => setIsHovered(false)}
     >
             <img src={'img/'+image} />
-            {/* hey {id}<br />
-            Are you selling a {medium}?<br />
-            and your name is: {artist}<br />
-            You made it in {year}<br />
-            it's {dimensions} big<br />
-            here's a pic: {image}<br /><br /> */}
-
-               {isHovered && (
-        <div className="hover-details">
-          <p>{artist} — {title}</p>
+            <p>{artist} — {title}</p>
           <p>{year}</p>
           <p>{medium}</p>
           <p>{dimensions}</p>
+
+               {isHovered && (
+        <div className="hover-details">
+          —————
         </div>
       )}
 
