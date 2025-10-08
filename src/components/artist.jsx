51e68e12d3da598ec423d2e97af5bfaf8e33746c
alisq <ali.shamas.qadeer@gmail.com>
2025-10-08 11:47:36 +0100

    minor tweaks.

--- src/components/artist.jsx
+++ src/components/artist.jsx
@@ -1,95 +1,87 @@
-import {useState} from "react";
+import { useState } from "react";
 
-function Artist({index, title, artist, id, year, medium, dimensions, image, bio, isOpen, onToggle}) {
-    const [isHovered, setIsHovered] = useState(false);
-    const [coords, setCoords] = useState({x:0, y:0})
-    
+function Artist({
+  index,
+  title,
+  artist,
+  id,
+  year,
+  medium,
+  dimensions,
+  image,
+  bio,
+  isOpen,
+  onToggle,
+}) {
+  const [isHovered, setIsHovered] = useState(false);
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
 
-     const handleMouseMove = (e) => {
-          setCoords({ x: e.clientX, y: e.clientY });
-        };
+  const handleMouseMove = (e) => {
+    setCoords({ x: e.clientX, y: e.clientY });
+  };
+
+  return (
+    <>
+      <tr
+        className={`artist-card ${isHovered ? "hovered" : ""} ${
+          isOpen ? "active" : ""
+        }`}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        onMouseMove={handleMouseMove}
+        onClick={onToggle}
+      >
+        <td>
+          <strong>{artist}</strong>
+        </td>
+        <td>
+          <strong>{title}</strong>
+        </td>
+        <td>{year}</td>
+        <td>{medium}</td>
+        <td>{dimensions}</td>
 
         
+        <td className="hideBig">
+        {image !=="" && (
+          <img src={"/img/small/" + image} alt={`${artist} - ${title}`} />
+        )}
+          <h3>----</h3>
+        </td>
         
-    return(
-      <>
-           <tr className={`artist-card ${isHovered ? "hovered" : ""} ${isOpen ? "active" : ""}`}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-            onMouseMove={handleMouseMove} 
-            onClick={onToggle} 
+
+        {isHovered && image !== "" && (
+          <div className="hover-details">
+            <div
+              className="hover-preview"
+              style={{
+                top: coords.y + 15, // offset so it’s not directly under cursor
+                left: coords.x + 15,
+              }}
             >
-      
-     
-            <td className="hideBig">
-            <strong> <img
-            src={'/img/small/'+image}
-            alt={`${artist} - ${title}`}
-            
-          /></strong>
-            </td>
-        
-            <td>
-            <strong>{artist}</strong>
-            </td>
-            <td><strong>{title}</strong>
-            </td>
-            <td>
-          {year}
-          </td>
-          <td>
-          {medium}
-          </td>
-          <td>
-          {dimensions}
-          </td>
+              <img src={"/img/small/" + image} alt={`${artist} - ${title}`} />
+            </div>
+          </div>
+        )}
+      </tr>
 
-               {(isHovered && (image !== "")) && (
-        <div className="hover-details">
-         <div
-          className="hover-preview"
-          style={{
-            
-            top: coords.y + 15,   // offset so it’s not directly under cursor
-            left: coords.x + 15
-            
-          }}
+      {image !== "" && (
+        <tr
+          className={`hidden_content ${isOpen ? "active" : ""}`}
+          onClick={onToggle}
         >
-          <img
-            src={'/img/small/'+image}
-            alt={`${artist} - ${title}`}
-            
-          />
-        </div>
-        </div>
-      )}
-
-
+          <td colSpan="10">
+            <div className="expanded">
+              <img src={`/img/big/${image}`} alt={`${artist} - ${title}`} />
+              <div>
+                <p>{bio}</p>
+              </div>
+            </div>
+          </td>
         </tr>
-
-       {image !== "" && (
-  <tr
-    className={`hidden_content ${isOpen ? "active" : ""}`}
-    onClick={onToggle}
-  >
-    <td colSpan="10">
-      <div className="expanded">
-        <img
-          src={`/img/big/${image}`}
-          alt={`${artist} - ${title}`}
-        />
-        <div>
-          <p>{bio}</p>
-        </div>
-      </div>
-    </td>
-  </tr>
-)}
-        
-        </>
-    )
+      )}
+    </>
+  );
 }
 
 export default Artist;
-
-
