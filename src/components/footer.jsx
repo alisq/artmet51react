8c2c03f79fcc5c58c9d8e7693695b9ef906681eb
alisq <ali.shamas.qadeer@gmail.com>
2025-10-11 08:50:29 +0100

    sponsors and kate newby

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,16 +4,22 @@ function Footer() {
         <section id="footer">
             <div className="footer_grid">
                 <div>
-<strong>Art Metropole</strong><br />
-896 College St<br />
-Toronto, Ontario<br />
-M6H 1A4 Canada<br />
-+1 416 703 4400
+                    <strong>Thank you to our event sponsors:</strong><br />
+                     <a href="https://collectiveartsbrewing.com/" target="_blank"  rel="noreferrer"><img className="sponsor_img" src="/img/sponsor_logos_01.png" /></a>
+    <a href="https://burdockbrewery.com/" target="_blank"  rel="noreferrer"><img className="sponsor_img" src="/img/sponsor_logos_02.png" /></a>
+    <a href="https://paradisegrapevine.com/" target="_blank"  rel="noreferrer"><img className="sponsor_img" src="/img/sponsor_logos_03.png" /></a>
+    <br />
+
 </div>
 
 <div>
-    © 2025 Art Metropole<br /><br />
-    Gossip font design by <a href="https://gossip.cargo.site/" target="_blank">Deb Khodanovich</a>.
+    
+   
+    <strong>© 2025 Art Metropole</strong><br />
+    896 College St<br />
+Toronto, Ontario, M6H 1A4<br />
++1 416 703 4400<br /><br />
+    Gossip font design by <a href="https://gossip.cargo.site/" target="_blank"><u>Deb Khodanovich</u></a>.
 </div>
 </div>
 
